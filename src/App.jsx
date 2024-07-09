598c83b9db4ec52df0b63dbda6a6a9728936d1be
Aaron Sanchez <saarong12@gmail.com>
2024-07-09 15:21:52 -0500

    Added auth0 demo to validate setup.
    Need to add testing to mock the login process and add service to validate connection to the backend.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import * as components from './components'
+import { Profile } from './profile'
 
 export const App = () => {
   return (
     <>
       <h1>Project Hercules</h1>
+      <components.LoginButton />
+      <components.LogoutButton />
+      <div>
+        <Profile />
+      </div>
     </>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import { LoginButton } from './login-button/LoginButton'
+import { LogoutButton } from './logout-button/LogoutButton'
+
+export { LoginButton, LogoutButton }
