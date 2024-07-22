ab267eba354fae7b37525c6be5070cf5de25a09d
Aaron Sanchez <saarong12@gmail.com>
2024-07-22 17:18:18 -0500

    Added MaterialUi and styled a basic drawer.

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react'
 import './Home.scss'
 import * as components from './components'
 import { useAuth0 } from '@auth0/auth0-react'
+import { Drawer } from '@mui/material'
 
 export const Home = () => {
+  const [open, setOpen] = useState(false)
   const { user } = useAuth0()
+
+  const handleOpen = (viewValue) => {
+    setOpen(!viewValue)
+  }
 
   return (
     <>
       <div className="home-page">
+        <button onClick={() => handleOpen(open)}>Menu</button>
+        <Drawer
+          className="side-bar"
+          open={open}
+          PaperProps={{ sx: { width: '45%' } }}
+          onClose={() => handleOpen(open)}
+        >
+          <p>Menu Here</p>
+        </Drawer>
         <div className="user-display">
           {!user ? (
             // TODO: Replace with default profile picture.
