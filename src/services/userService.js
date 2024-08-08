1ecc5f9f31f5ad6c13dac1428ad246eeccd30451
Aaron Sanchez <saarong12@gmail.com>
2024-08-08 17:57:46 -0500

    Created service call to save a workout to the database.

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,4 @@
+/** @desc Fetch user info from the database. */
 export const fetchUserDetails = async (accessToken, user) => {
   // Create new header with user info.
   const customHeaders = {
@@ -15,6 +16,28 @@ export const fetchUserDetails = async (accessToken, user) => {
         ...customHeaders
       }
     })
+
+    const json = await response.json()
+    return json
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+/** @desc Save a workout to the database. */
+export const saveWorkout = async (accessToken, userId, workout) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3001/api/v1/workouts/user/${userId}`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(workout)
+      }
+    )
 
     const json = await response.json()
     return json
