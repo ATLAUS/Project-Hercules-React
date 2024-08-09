73de90a5a61eab763f462430e6da8736cc3e663f
Aaron Sanchez <saarong12@gmail.com>
2024-08-09 11:11:08 -0500

    Added unit test for the save dialog modal.

--- __tests__/routes/workout/components/SaveDialog.test.jsx
+++ __tests__/routes/workout/components/SaveDialog.test.jsx
@@ -1,8 +1,12 @@
 import { describe, test, expect } from 'vitest'
-// import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react'
+import { SaveDialog } from '../../../../src/routes/workout/components'
 
 describe('SaveDialog Component', () => {
   test('Should render the SaveDialog component', () => {
-    expect(true).toBe(true)
+    render(<SaveDialog open={true} handleClose={() => {}} />)
+
+    const dialog = screen.getByText(/name your workout/i)
+    expect(dialog).toBeInTheDocument()
   })
 })
