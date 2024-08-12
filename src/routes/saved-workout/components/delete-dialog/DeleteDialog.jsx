e0db83bd3986145083fed195e08cc0b0806d9584
Aaron Sanchez <saarong12@gmail.com>
2024-08-12 16:18:12 -0500

    Connected deletion service to the delete dialog on the saved workout page.

--- src/routes/saved-workout/components/delete-dialog/DeleteDialog.jsx
+++ src/routes/saved-workout/components/delete-dialog/DeleteDialog.jsx
@@ -5,10 +5,24 @@ import DialogContent from '@mui/material/DialogContent'
 import DialogActions from '@mui/material/DialogActions'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
+import { useAuth0 } from '@auth0/auth0-react'
+import { DeleteWorkoutByID } from '../../../../services/UserService'
+import { useNavigate } from 'react-router-dom'
 
 export const DeleteDialog = ({ open, handleClose, workoutID }) => {
-  const deleteWorkout = () => {
+  const navigate = useNavigate()
+
+  const { getAccessTokenSilently } = useAuth0()
+
+  const deleteWorkout = async () => {
     console.log(`Delete %s`, workoutID)
+    try {
+      const accessToken = await getAccessTokenSilently()
+      await DeleteWorkoutByID(accessToken, workoutID)
+      navigate('/home')
+    } catch (error) {
+      console.log(error.message)
+    }
   }
   return (
     <>
