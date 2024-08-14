f7e8f037bf61f2a07936439228ff6cbcb62516de
ghezaii <ghezai@google.com>
2024-08-14 12:40:47 -0400

    Tested rendering of landing page

---
+++ __tests__/routes/landing/Landing.test.jsx
@@ -0,0 +1,28 @@
+import { describe, test, expect, vi, beforeEach } from 'vitest'
+import { render, screen, waitFor } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { Landing } from '../../../src/routes/landing/Landing'
+
+describe('Landing page', () => {
+    test('should render landing page', () => {
+        render(
+            <MemoryRouter initialEntries={['/']}>
+                <Landing />
+            </MemoryRouter>
+        )
+
+        const landingTitle = screen.getByRole('heading', { name: /Project Hercules/i })
+        expect(landingTitle).toBeInTheDocument()
+    })
+    
+    test('should render previous user prompt', () => {
+        render(
+            <MemoryRouter initialEntries={['/']}>
+                <Landing />
+            </MemoryRouter>
+        )
+
+        const previousUserPrompt = screen.getByText('ALREADY HAVE AN ACCOUNT?')
+        expect(previousUserPrompt).toBeInTheDocument()
+    })
+})
