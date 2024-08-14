d59b3118ec4fe51fc2f91f04cec9523d64154248
Daniel Guerrero <danxguerrero@yahoo.com>
2024-08-14 19:01:11 -0400

    Removed unused Home Icon import

--- src/routes/form/Form.jsx
+++ src/routes/form/Form.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { Button, IconButton, ToggleButton, ToggleButtonGroup } from '@mui/material'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
-import HomeIcon from '@mui/icons-material/Home'
 import { useAuth0 } from '@auth0/auth0-react'
 import { fetchNewGeminiWorkout } from '../../services/GeminiService'
 
