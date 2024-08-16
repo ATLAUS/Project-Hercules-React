1a88a05f3ee9e24d2c1899d670a5af6f81e85364
ghezaii <ghezai@google.com>
2024-08-16 12:28:12 -0400

    Abbreviated app name

--- src/routes/landing/Landing.jsx
+++ src/routes/landing/Landing.jsx
@@ -12,7 +12,7 @@ export const Landing = () => {
             <span className='subtext-string'>Powered by </span>
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Google_Gemini_logo.svg/1280px-Google_Gemini_logo.svg.png' alt='Gemini' className='subtext-gemini-logo'/>
             <div className='subtext-blurb'>
-              <p>Want to create workouts for everyday use? Project Hercules has you covered!</p>
+              <p>Want to create workouts for everyday use? PH has you covered!</p>
               <p><b>Create Workouts</b> : Generated a workout powered by Gemini API that is tailored to your specifications</p>
               <p><b>View and Delete your Workouts </b> : View your workouts and customize your saved workouts to your liking</p>
             </div>
